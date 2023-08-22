abb51ba79882b38f01d90bc42d2778cb7e6df972
warren yun <wyun2006@gmail.com>
2023-08-22 00:54:11 -0400

    basic card component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={`flex  min-h-screen flex-col  ${inter.className}`}>
+    <main className={`min-h-screen   ${inter.className}`}>
       <div className="bg-zinc-900 flex items-center justify-center py-6">
         <Image src={"/trace.svg"} alt="logo" width={320} height={320} />
       </div>
-      <div className="flex items-center justify-center py-8">
-        <div className="">
-          <h1>Hello</h1>
-        </div>
+      <div className="grid grid-rows-3 gap-12 w-full py-8 px-8">
+        <article className="h-full flex border shadow-xl  border-zinc-800">
+          <div className="h-full px-4 py-6 pb-12 ">
+            <h1 className="block w-full  mb-4 text-3xl font-semibold font-inte">
+              hi
+            </h1>
+            <p>hello</p>
+          </div>
+
+          <header className="flex items-center justify-between p-2 leading-tight w-full  bg-zinc-800 md:p-4">
+            <div className="no-underline">Boo</div>
+          </header>
+        </article>
       </div>
     </main>
   );
