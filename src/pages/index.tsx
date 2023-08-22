a7f4130ea0d9b2ded73856ddb1e342670e57279d
warren yun <wyun2006@gmail.com>
2023-08-22 01:22:38 -0400

    nicer looking cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { FaTram } from "react-icons/fa";
+import { IoMdTrain } from "react-icons/io";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,17 +11,31 @@ export default function Home() {
       <div className="bg-zinc-900 flex items-center justify-center py-6">
         <Image src={"/trace.svg"} alt="logo" width={320} height={320} />
       </div>
-      <div className="grid grid-rows-3 gap-12 w-full py-8 px-8">
-        <article className="h-full flex border shadow-xl  border-zinc-800">
-          <div className="h-full px-4 py-6 pb-12 ">
-            <h1 className="block w-full  mb-4 text-3xl font-semibold font-inte">
-              hi
+      <div className="grid grid-rows-3 gap-10 w-full py-8 px-4">
+        <article className="h-full flex  shadow-lg rounded-2xl border-zinc-800">
+          <div className="h-full px-6 py-16 bg-red-500 rounded-l-2xl">
+            <h1 className="block w-full text-4xl  text-white font-semibold font-inter">
+              <FaTram />
             </h1>
-            <p>hello</p>
           </div>
 
-          <header className="flex items-center justify-between p-2 leading-tight w-full  bg-zinc-800 md:p-4">
-            <div className="no-underline">Boo</div>
+          <header className="flex py-3 px-3 leading-tight w-full rounded-r-2xl bg-zinc-100 md:p-4">
+            <h1 className="font-semibold pb-2 text-lg">
+              Roosevelt Island Tram
+            </h1>
+          </header>
+        </article>
+        <article className="h-full flex flex-row-reverse  shadow-lg rounded-2xl border-zinc-800">
+          <div className="h-full px-6 py-16 bg-orange-500 rounded-r-2xl text-right">
+            <h1 className=" w-full text-4xl text-white font-semibold font-inter flex items-center justify-center">
+              <IoMdTrain />
+            </h1>
+          </div>
+
+          <header className="flex justify-end py-3 px-3 leading-tight w-full rounded-l-2xl  bg-zinc-100 md:p-4">
+            <h1 className="font-semibold pb-2 text-lg">
+              Lexington Ave. & 63rd St
+            </h1>
           </header>
         </article>
       </div>
