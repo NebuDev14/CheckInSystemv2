42b2e2e68a0e3950569e40868af9fd8f8135c614
warren yun <wyun2006@gmail.com>
2023-08-22 04:49:51 -0400

    small ui stuff

--- src/components/Trains.tsx
+++ src/components/Trains.tsx
@@ -1,9 +1,7 @@
 import { IoMdTrain } from "react-icons/io";
 import { Stop } from "@/pages";
 
-export const Trains: React.FC<{ trains: Stop[] }> = ({
-  trains
-}) => {
+export const Trains: React.FC<{ trains: Stop[] }> = ({ trains }) => {
   return (
     <div>
       <article className="h-full flex flex-row-reverse  shadow-lg rounded-2xl border-zinc-800">
@@ -13,10 +11,19 @@ export const Trains: React.FC<{ trains: Stop[] }> = ({
           </h1>
         </div>
 
-        <header className="flex justify-end py-3 px-4 leading-tight w-full rounded-l-2xl  bg-zinc-100 ">
-          <h1 className="font-semibold pb-2 text-lg">
+        <header className="flex flex-col justify-end py-3 px-4 leading-tight w-full rounded-l-2xl  bg-zinc-100 ">
+          <h1 className="font-semibold pb-2 text-lg text-right mb-3">
             Lexington Av. & 59th St
           </h1>
+          <div className="grid h-full grid-cols-2 gap-2">
+            <div className="flex flex-col items-center justify-start border-r-2 border-r-zinc-300">
+              <h1 className="font-semibold">Uptown / Queens</h1>
+            </div>
+
+            <div className="flex flex-col items-center justify-start ">
+              <h1 className="font-semibold">Downtown / Manhattan</h1>
+            </div>
+          </div>
         </header>
       </article>
     </div>
