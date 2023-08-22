9e9a5986ac312650169f8449fd417a29d6c6094b
warren yun <wyun2006@gmail.com>
2023-08-22 19:56:28 -0400

    todays changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { FaTram } from "react-icons/fa";
+import { MdDirectionsBoat } from "react-icons/md";
 import { Trains } from "@/components/Trains";
 import useSWR from "swr";
 
@@ -22,6 +23,8 @@ export default function Home() {
     fetcher,
     { refreshInterval: 30000 }
   );
+  
+  console.log(trainData)
 
   const { data: tramData } = useSWR(
     "http://localhost:3000/api/tram",
@@ -47,33 +50,51 @@ export default function Home() {
   return (
     <main className={`min-h-screen   ${inter.className}`}>
       <div className="bg-zinc-900 flex items-center justify-center py-6">
-        <Image src={"/trace.svg"} alt="logo" width={275} height={275} />
+        <Image src={"/trace.svg"} alt="logo" width={650} height={650} />
       </div>
       <div className="flex flex-col w-full p-4">
-        <article className="h-full mb-6 flex  shadow-lg rounded-2xl border-zinc-800">
-          <div className="h-full px-6 py-16 bg-red-500 rounded-l-2xl">
-            <h1 className="block w-full text-4xl  text-white font-semibold font-inter">
-              <FaTram />
-            </h1>
-          </div>
-
-          <header className="flex flex-col py-3 px-4 leading-tight w-full rounded-r-2xl bg-zinc-100 ">
-            <h1 className="font-semibold text-lg mb-3">
-              Roosevelt Island Tram
-            </h1>
-            <div className="grid h-full grid-cols-2 gap-2">
-              <div className="flex flex-col items-center justify-center border-r-2 border-r-zinc-300">
-                <h1 className="text-5xl font-bold">15</h1>
-                <h1 className="text-xl ">minutes</h1>
-              </div>
-              <div className="px-4 py-2 text-center">
-                <h1 className="text-lg text-center">6:07 PM</h1>
-                <h1 className="text-lg">6:15 PM</h1>
-                <h1 className="text-lg">┋</h1>
-              </div>
+        <div className="grid grid-cols-2 gap-8 mt-4 mb-8">
+          <article className="h-full mb-6 flex shadow-lg rounded-xl border-zinc-800 bg-red-500">
+            <div className="h-full px-4 my-auto  bg-red-500 rounded-l-xl flex items-center justify-center">
+              <h1 className="text-white flex items-center justify-center font-semibold font-inter bg-red-500">
+                <FaTram size={45} />
+              </h1>
             </div>
-          </header>
-        </article>
+
+            <header className="flex flex-col px-4 items-center justify-center leading-tight w-full rounded-r-xl bg-zinc-100 ">
+              <div className="py-6">
+                <div className="flex flex-col items-center justify-center ">
+                  <h1 className="text-8xl font-bold">15</h1>
+                  <h1 className="text-4xl mb-4">minutes</h1>
+                  <div className="grid grid-cols-2 gap-3">
+                      <h1 className="text-2xl pr-2 border-r-2 border-r-zinc-400">19</h1>
+                      <h1 className="text-2xl">22</h1>
+                  </div>
+                </div>
+              </div>
+            </header>
+          </article>
+          <article className="h-full mb-6 flex shadow-lg rounded-xl border-zinc-800 bg-cyan-400">
+            <div className="h-full px-4 my-auto  bg-cyan-400 rounded-l-xl flex items-center justify-center">
+              <h1 className="text-white flex items-center justify-center font-semibold font-inter bg-cyan-400">
+                <MdDirectionsBoat size={45} />
+              </h1>
+            </div>
+
+            <header className="flex flex-col px-4 items-center justify-center leading-tight w-full rounded-r-xl bg-zinc-100 ">
+              <div className="py-6">
+                <div className="flex flex-col items-center justify-center ">
+                  <h1 className="text-8xl font-bold">4</h1>
+                  <h1 className="text-4xl mb-4">minutes</h1>
+                  <div className="grid grid-cols-2 gap-3">
+                      <h1 className="text-2xl pr-2 border-r-2 border-r-zinc-400">9</h1>
+                      <h1 className="text-2xl">17</h1>
+                  </div>
+                </div>
+              </div>
+            </header>
+          </article>
+        </div>
         <Trains trains={stops} />
       </div>
     </main>
