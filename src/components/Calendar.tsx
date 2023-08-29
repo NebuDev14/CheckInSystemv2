b51478c60b4e3c5b95eec1c7a364bf2bb3b22fed
warren yun <wyun2006@gmail.com>
2023-08-29 17:19:53 -0400

    i am going to epxleo

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -22,16 +22,14 @@ export const Calendar: React.FC = () => {
     }
   );
 
-  console.log(data);
-
   return (
-    <div className=" col-span-6 text-4xl text-black rounded-2xl p-8 bg-white shadow-lg border-zinc-400 border  items-center justify-center">
+    <div className=" col-span-6 text-4xl text-black rounded-2xl py-8 bg-white shadow-lg border-zinc-400 border  items-center justify-center">
       {/* <h1 className="text-4xl text-red-500 ">
         {dayName[new Date().getDay()].toUpperCase()}
       </h1>
       <h1 className="text-6xl mt-2 mb-4">{new Date().getUTCDate()}</h1> */}
       <div className="flex flex-col">
-        {data?.map((event: any) => (
+        {/* {data?.map((event: any) => (
           <div className="flex px-1 my-1">
             <h1>
               {new Date(event.date as string).toLocaleTimeString("en-US", {
@@ -39,6 +37,18 @@ export const Calendar: React.FC = () => {
                 timeStyle: "short",
               })}
             </h1>
+          </div>
+        ))} */}
+        {[...Array(3)].map((_, i) => (
+          <div className="my-8">
+            -{" "}
+            {new Date(new Date().getTime() + 3.6e6 * i).getHours()%12 + new Date(new Date().getTime() + 3.6e6 * i).toLocaleTimeString(
+              "en-US",
+              {
+                hour12: true,
+                timeStyle: "short",
+              }
+            ).slice(-2)}
           </div>
         ))}
       </div>
